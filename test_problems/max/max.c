881a8c56955f04f077eec99da821333e8eb3b4bc
SevenhuijsenM <64038833+SevenhuijsenM@users.noreply.github.com>
2024-02-01 08:34:50 +0100

    Updated the swap example

--- test_problems/max/max.c
+++ test_problems/max/max.c
@@ -1,5 +1,6 @@
 // Create a function that returns the maximum of two integers
-/*@     ensures \result >= x && \result >= y;
+/*@     assigns \nothing;
+        ensures \result >= x && \result >= y;
         ensures \result == x || \result == y;
 */
 int max(int x, int y) { return (x > y) ? x : y; }
