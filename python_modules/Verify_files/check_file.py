d821cd58389c5fcd17b695614913d3bb2cbbba2e
Merlijn <merlijn.sevenhuijsen@studenti.unitn.it>
2024-02-07 12:59:23 +0100

    Update check_file.py

--- python_modules/Verify_files/check_file.py
+++ python_modules/Verify_files/check_file.py
@@ -9,22 +9,22 @@ from Verify_files.verify_file import verify_file
 # @return None
 
 # Checks a file in a given directory
-def check_file(args, file_c, file_h):
+def check_file(args, path_file_c, path_file_h):
     # Get juts the name of the C and the header file
-    file_name_c = file_c.split("/")[-1]
-    file_name_h = file_h.split("/")[-1]
+    file_name_c = path_file_c.split("/")[-1]
+    file_name_h = path_file_h.split("/")[-1]
     
     print(f"Files {file_name_c} and {file_name_h} exists, starting to compile...")
 
     # Compile the file
-    result, _, output = compile_c(file_c)
+    result, _, output = compile_c(path_file_c)
     if result is False:
-        print(f"Compilation of file {file_c} failed, Error:\n {output}")
+        print(f"Compilation of file {file_name_c} failed, Error:\n {output}")
         sys.exit()
     else:
-        print(f"File {file_c} compiled successfully")
+        print(f"File {file_name_c} compiled successfully")
 
-    print(f"File {file_c} will be verified...")
+    print(f"File {file_name_c} will be verified...")
 
     # Verify the file
-    result, _ = verify_file(args, file_c, file_h)
+    return verify_file(args, path_file_c, path_file_h)
