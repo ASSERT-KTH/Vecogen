e0884449dffb92cd832eeea49ddcbca86aafdabd
Merlijn <merlijn.sevenhuijsen@studenti.unitn.it>
2024-04-03 06:53:21 +0200

    update specification vasaya the hipster

--- no_loop_problems/0/specification-fs.h
+++ no_loop_problems/0/specification-fs.h
@@ -1,18 +1,11 @@
 /*@
+    logic variable
     requires \valid(out_1) && \valid(out_2) && \separated(out_1, out_2);
     requires 1 <= a <= 100;
     requires 1 <= b <= 100;
     assigns *out_1, *out_2;
-    behavior has_more_red:
-        assumes a > b;
-        ensures *out_1 == b && *out_2 == (a - b) / 2;
-    behavior has_more_blue:
-        assumes b > a;
-        ensures *out_1 == a && *out_2 == (b - a) / 2;
-    behavior equal_socks:
-        assumes a == b;
-        ensures *out_1 == a && *out_2 == 0;
-    complete behaviors;
-    disjoint behaviors;
+    ensures 2 * n_aa + n_ab = a;
+    ensures 2 * n_bb + n_ab = b;
+
 */
 void calculateHipsterSockDays(int a, int b, int *out_1, int *out_2);
