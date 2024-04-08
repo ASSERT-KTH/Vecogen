9580fc13839570f29a415492438fbce178a644d5
SevenhuijsenM <64038833+SevenhuijsenM@users.noreply.github.com>
2024-04-08 13:33:32 +0200

    updated 757 formal specification

--- no_loop_problems/757/specification-fs.h
+++ no_loop_problems/757/specification-fs.h
@@ -1,3 +1,17 @@
+/*@ predicate validSolution(integer n, integer k, integer l, integer c, integer d, integer p, integer nl, integer np, integer out) =
+    c * d >= out * n &&
+    k * l >= out * n * nl &&
+    p >= out * n * np;
+*/
+
+/*@ predicate existLargerSolution(integer n, integer k, integer l, integer c, integer d, integer p, integer nl, integer np, integer out) =
+    \exists integer x;
+    c * d >= x * n &&
+    k * l >= x * n * nl &&
+    p >= x * n * np &&
+    x > out;
+*/
+
 /*@
     requires \valid(out);
     requires 1 <= n <= 1000;
@@ -9,7 +23,7 @@
     requires 1 <= nl <= 1000;
     requires 1 <= np <= 1000;
     assigns *out;
-    ensures *out == \min((k * l) / nl, \min(c * d, p / np)) / n;
-    ensures *out >= 0;
+    ensures validSolution(n, k, l, c, d, p, nl, np, *out);
+    ensures !existLargerSolution(n, k, l, c, d, p, nl, np, *out);
 */
 void calculateMaximumToastsPerFriend(int n, int k, int l, int c, int d, int p, int nl, int np, int *out);
