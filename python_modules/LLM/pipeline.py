06588b1f9655bbdc04655dbe373a04680afea0a9
Merlijn <merlijn.sevenhuijsen@studenti.unitn.it>
2024-05-22 16:17:49 +0200

    refarctored code, added initial examples
    Refactored the code to have less in one function

--- python_modules/LLM/pipeline.py
+++ python_modules/LLM/pipeline.py
@@ -14,11 +14,7 @@ def generate_code(args, improve = False, print_information_iteration = True):
     Args:
         args: The arguments given to the program
         improve: Boolean that indicates if the code is improved
-    Returns:
-        None
-        
-    Requirements of the arguments:
-    - The output path is absolute
+        print_information_iteration: Boolean that indicates if the information about the iteration should be printed
     """
 
     # Create an array to store the information about the iterations
@@ -30,61 +26,14 @@ def generate_code(args, improve = False, print_information_iteration = True):
 
     # Check if the model continues the code or starts from scratch
     if improve:
-        # Get the code from the c file
-        with open(args.c_file, "r", encoding="utf-8") as f:
-            code = f.read()
-
-        # Write the c code to the output path
-        with open(f"{args.absolute_output_directory}/{args.output_file}", "w",
-                encoding="utf-8") as f:
-            f.write(code)
-
-        # Verify the file
-        verified, output, _ = check_file(args.absolute_c_path, args.absolute_header_path, args)
-
-        # Get the output path
-        prompt = verification_error_prompt(args.header_file, code, output, \
-                    args.model_name, args.max_tokens, args.allowloops)
+        prompt, output, verified = improve_code_prompt(args)
     else:
         prompt = initial_prompt(args.header_file, args.model_name, args.max_tokens, args.allowloops)
 
     # generate the initial attempts by making prompts of at most x each
-    responses_gpt = []
-    tokens_used = []
-    model_used = []
-    i_examples_generated = 0
-    max_examples_per_call = 10
-    
-    # Make GPT requests with at most x examples at a time
-    while i_examples_generated < args.initial_examples_generated:
-        # Calculate the amount of examples to generate
-        n = min(max_examples_per_call, args.initial_examples_generated - i_examples_generated)
-        
-        # Call the function to make the GPT request
-        response_gpt, tokens_used_call, model_used = make_gpt_request(args, prompt, n)
-        
-        # Add the tokens used to the list
-        # Only the first iteration has the tokens used
-        tokens_used_initial_n_examples = [0] * (n - 1)
-        tokens_used_initial_n_examples.insert(0, tokens_used_call)
-        
-        # Add the tokens used and the responses to the list
-        tokens_used.extend(tokens_used_initial_n_examples)
-        responses_gpt.extend(response_gpt)
-        
-        # Increase the counter
-        i_examples_generated += n
+    responses_gpt, tokens_used, model_used = prompt_using_n_examples(args, prompt, 10000)
 
-    # Print the initial examples and tokens used
-    if args.debug:
-        print("Initial examples and tokens used:")
-        for i in range(args.initial_examples_generated):
-            print(f"Example {i+1} of {args.initial_examples_generated}")
-            print(f"Tokens used: {tokens_used[i]}")
-            print(responses_gpt[i].message.content)
-            print("-" * 50)
-            print("\n")
-    # For each response, check the code
+    # For each response, check the code. Stop if the code is verified
     for i in range(args.initial_examples_generated):
         print("-" * 50)
         print(f"Initial iteration {i+1} of {args.initial_examples_generated}, generating code...")
@@ -94,41 +43,17 @@ def generate_code(args, improve = False, print_information_iteration = True):
         response_gpt = responses_gpt[i].message.content
 
         # Process the generated code
-        try:
-            code, verified, output, verified_goals, test_information = \
-                verify_and_test_code_attempt(args, response_gpt, i)
+        code, verified, output, verified_goals, iteration_info, verified_percentage = \
+            process_code_and_get_iteration_information(args, response_gpt, i, prompt, tokens_used[i], model_used, True)
 
-        except IndexError:
-            print("The code could not be generated, please try again.")
-            verified, output, verified_goals = False, "The model did not generate code", "0/0"
-            break
-
-        # Create a dict that will contain information about the iteration
-        iteration_info = {
-            "iteration": i,
-            "prompt": prompt,
-            "gpt_output": response_gpt,
-            "verified": verified,
-            "verified_goals": verified_goals,
-            "test_information": test_information,
-            "temperature": args.temperature,
-            "info": "initial prompt",
-            "max_tokens": args.max_tokens,
-            "tokens_used": tokens_used[i],
-            "model": model_used,
-        }
+        # Add extra information about the generation attempt
         information_iteration.append(iteration_info)
-
-        # Get the percentage of verified goals
-        total_goals = verified_goals.split("/")[1]
-        verified_goals = verified_goals.split("/")[0]
-        if int(total_goals) == 0:
-            verified_percentage = 0
-        else:
-            verified_percentage = int(verified_goals) / int(total_goals)
-
         initial_generation_attempts.append([verified_percentage, response_gpt, verified, output])
-
+        
+        # If the code is verified, then break
+        if verified:
+            break
+    
     # Pick the best initial generation attempt
     if args.initial_examples_generated > 1:
         best_attempt = max(initial_generation_attempts, key = lambda x: x[0])
@@ -142,6 +67,7 @@ def generate_code(args, improve = False, print_information_iteration = True):
 
     # Generate a prompt
     i = args.initial_examples_generated
+    i_total = args.initial_examples_generated
     i_reboot = 0
 
     # If there is an improvement step then get the prompt
@@ -153,50 +79,27 @@ def generate_code(args, improve = False, print_information_iteration = True):
     while (i < args.initial_examples_generated + args.iterations and not verified):
         if print_information_iteration:
             print("-" * 50)
-            print(f"Improvement Iteration {i} of {args.iterations}, generating code...")
+            print(f"Improvement Iteration {i - args.initial_examples_generated} of {args.iterations}, generating {args.initial_examples_generated} fixes...")
             print("-" * 50)
 
         # Get the output from the LLM
-        response_gpt, tokens_used, model_used = make_gpt_request(args, prompt, 1)
+        responses_gpt, tokens_used, model_used = prompt_using_n_examples(args, prompt, 10000)
 
-        # Take the first response
-        response_gpt = response_gpt[0].message.content
+        for j in range(args.initial_examples_generated):
+            response_gpt = responses_gpt[j].message.content
 
-        # Process the generated code
-        try:
-            code, verified, output, verified_goals, test_information = \
-                verify_and_test_code_attempt(args, response_gpt, i)
+            # Process the generated code
+            code, verified, output, verified_goals, iteration_info, verified_percentage = \
+                process_code_and_get_iteration_information(args, response_gpt, i_total, prompt, tokens_used[j], model_used, \
+                rebooting= args.reboot == i_reboot)
+                
+            i_total += 1
+            information_iteration.append(iteration_info)
+            
+            if verified:
+                break
 
-        except IndexError as e:
-            print("The code could not be generated, please try again. The error is: ", e)
-            verified, output, verified_goals = False, "The model did not generate code", "0/0"
-            break
-
-        # Add extra information about the generation attempt
-        if i_reboot == args.reboot:
-            information = "rebooted"
-        elif verified:
-            information = "verified"
-        else:
-            information = "code improved"
-
-        # Create a dict that will contain information about the iteration
-        iteration_info = {
-            "iteration": i,
-            "prompt": prompt,
-            "gpt_output": response_gpt,
-            "verified": verified,
-            "verified_goals": verified_goals,
-            "test_information": test_information,
-            "temperature": args.temperature,
-            "info": information,
-            "max_tokens": args.max_tokens,
-            "tokens_used": tokens_used,
-            "model": model_used,
-        }
-        information_iteration.append(iteration_info)
-
-        # Check if the code needs to be rebooted
+        # Get the next prompt
         if not verified and i_reboot == args.reboot:
             if args.debug:
                 print("Code has not been verified, rebooting...")
@@ -226,7 +129,7 @@ def generate_code(args, improve = False, print_information_iteration = True):
         json.dump(information_iteration, f, indent=4)
 
 # Function that processes the generated code by adding the specification and creating the output file
-def process_generated_code(args, code):
+def add_specification_and_output_code(args, code):
     """Function to process the generated code and write it to a file
     Args:
         args: The arguments given to the program
@@ -277,6 +180,9 @@ def generate_code_folder(args):
     
     # Sort the folders based on the number
     folders.sort(key=lambda x: int(x.split('-')[0]))
+    
+    # Take only the folders larger than n
+    # folders = [f for f in folders if int(f.split('-')[0]) <= 200]
     
     # For each folder in the directory
     for folder in folders:
@@ -335,7 +241,7 @@ def verify_and_test_code_attempt(args, response_gpt, i):
     """
 
     # Process the generated code by adding the specification
-    code = process_generated_code(args, response_gpt)
+    code = add_specification_and_output_code(args, response_gpt)
 
     # Verify the code
     verified, output, verified_goals = check_file(args.absolute_c_path,
@@ -372,4 +278,138 @@ def verify_and_test_code_attempt(args, response_gpt, i):
             print(f"No tests found, proved goals: {verified_goals}")
 
     return code, verified, output, verified_goals, test_information
-__all__ = ["generate_code", "generate_code_folder"]
+
+# Function that creates a prompt based on existing code
+def improve_code_prompt(args):
+    """Function to create a prompt based on the verification error
+    Args:
+        args: The arguments given to the program
+    Returns:
+        verified: Boolean that indicates if the code is verified
+        output: The output of the verification process
+        prompt: The prompt as a string
+    """
+
+    # Get the code from the c file
+    with open(args.c_file, "r", encoding="utf-8") as f:
+        code = f.read()
+
+    # Write the c code to the output path
+    with open(f"{args.absolute_output_directory}/{args.output_file}", "w",
+            encoding="utf-8") as f:
+        f.write(code)
+
+    # Verify the file
+    verified, output, _ = check_file(args.absolute_c_path, args.absolute_header_path, args)
+
+    # Get the output path
+    prompt = verification_error_prompt(args.header_file, code, output, \
+                args.model_name, args.max_tokens, args.allowloops)
+    
+    return verified, output, prompt
+
+# Function that uses the LLM to generate code, whilst only asking for N exmaples at a time
+def prompt_using_n_examples(args, prompt, n):
+    """Function to generate code using the pipeline and the LLM model
+    Args:
+        args: The arguments given to the program
+        prompt: The prompt to use
+    Returns:
+        responses_gpt: The responses from the GPT model
+        tokens_used: The amount of tokens used for each response
+        model_used: The model used, no list is used as only one model is used
+    """
+    
+    # generate the initial attempts by making prompts of at most x each
+    responses_gpt = []
+    tokens_used = []
+    model_used = []
+    i_examples_generated = 0
+    
+    # Make GPT requests with at most n examples at a time
+    while i_examples_generated < args.initial_examples_generated:
+        # Calculate the amount of examples to generate
+        n = min(n, args.initial_examples_generated - i_examples_generated)
+        
+        # Call the function to make the GPT request
+        response_gpt, tokens_used_call, model_used = make_gpt_request(args, prompt, n)
+        
+        # Add the tokens used to the list
+        # Only the first iteration has the tokens used
+        tokens_used_initial_n_examples = [0] * (n - 1)
+        tokens_used_initial_n_examples.insert(0, tokens_used_call)
+        
+        # Add the tokens used and the responses to the list
+        tokens_used.extend(tokens_used_initial_n_examples)
+        responses_gpt.extend(response_gpt)
+        
+        # Increase the counter
+        i_examples_generated += n
+    
+    return responses_gpt, tokens_used, model_used
+
+# Function that processes the code, and gets iteration information, and verifies the goals
+def process_code_and_get_iteration_information(args, response_gpt, i, prompt, 
+            tokens_used, model_used, initial_attempt = False, rebooting = False):
+    """Function to process the generated code and get iteration information
+    Args:
+        args: The arguments given to the program
+        response_gpt: The response from the GPT model
+        i: The iteration number
+        prompt: The prompt that was used
+        tokens_used: The amount of tokens used for each response
+        model_used: The model used for each response
+    Returns:
+        code: The code that has been generated
+        verified: Boolean that indicates if the code is verified
+        output: The output of the verification process
+        verified_goals: The proportion of goals that have been verified
+        test_information: The information about the tests
+        iteration_info: The information about the iteration
+        verified_percentage: The percentage of verified goals
+    """
+
+    # Process the generated code
+    try:
+        code, verified, output, verified_goals, test_information = \
+            verify_and_test_code_attempt(args, response_gpt, i)
+
+    except IndexError as e:
+        print("The code could not be generated, please try again.")
+        verified, output, verified_goals = False, f"The model did not generate code, error {e}", "0/0"
+
+     # Add extra information about the generation attempt
+    if initial_attempt:
+        information = "initial attempt"
+    elif rebooting:
+        information = "rebooted"
+    elif verified:
+        information = "verified"
+    else:
+        information = "code improved"
+
+    # Create a dict that will contain information about the iteration
+    iteration_info = {
+        "iteration": i,
+        "prompt": prompt,
+        "gpt_output": response_gpt,
+        "verified": verified,
+        "verified_goals": verified_goals,
+        "test_information": test_information,
+        "temperature": args.temperature,
+        "info": information,
+        "max_tokens": args.max_tokens,
+        "tokens_used": tokens_used,
+        "model": model_used,
+    }
+
+    # Get the percentage of verified goals
+    total_goals = verified_goals.split("/")[1]
+    verified_goals = verified_goals.split("/")[0]
+    if int(total_goals) == 0:
+        verified_percentage = 0
+    else:
+        verified_percentage = int(verified_goals) / int(total_goals)
+        
+    return code, verified, output, verified_goals, iteration_info, verified_percentage
+
