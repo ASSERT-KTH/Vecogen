c9de1ffccd9604a90cf100afb97ff2f024196bab
Merlijn <merlijn.sevenhuijsen@studenti.unitn.it>
2024-12-04 13:33:54 +0100

    Delete CodeLlama.py

--- python_modules/LLM/CodeLlama.py
+++
@@ -1,9 +0,0 @@
-import os
-import json
-import logging
-from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
-import gc
-from LLM.prompts import seperate_prompt
-from LLM.AbstractLLM import LLM
-# Module inspired by https://github.com/ASSERT-KTH/MoKav/blob/feat/codellama/src/chatgpt.py#L80
-# This is used to run CodeLama on a GPU
